0d1b2a4435158669ce7b3ca7199663efa10b72fb
Tejas Chaudhari <tmcv0.01@gmail.com>
2023-10-11 20:22:57 +0200

    Add first code

---
+++ class_practice.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+class student{
+    public:
+    string name;
+    int age;
+    bool gender;
+};
+
+int main(){
+    student arr[3];
+    for(int i=0;i<sizeof(arr);i++){
+        
+    }
+    return 0;
+}
