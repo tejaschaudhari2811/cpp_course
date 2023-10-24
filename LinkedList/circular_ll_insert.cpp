00641c0ad2c91b1037779ba834a1dcee6a4535ee
Tejas Chaudhari <tmcv0.01@gmail.com>
2023-10-24 10:54:04 +0200

    Add code for insert in Circular LList

--- LinkedList/circular_ll_insert.cpp
+++ LinkedList/circular_ll_insert.cpp
@@ -21,6 +21,34 @@ void Display(node* head){
 
 }
 
+void Insert(node* &head, int position, int new_node_value){
+    node* p = head;
+    node* temp = new node(new_node_value);
+    
+    if(position == 0){
+        if(head == NULL){
+            head = temp;
+            head->next = head;
+        }
+        else {
+            do{
+                p = p->next;
+            } while (p->next != head);
+            p->next = temp;
+            temp->next = head;
+            head = temp; 
+        }
+    }
+    else {
+        for(int i = 0; i < position - 1; i++){
+            p = p->next;
+        }
+        temp->next = p->next;
+        p->next = temp;
+    }
+}
+
+
 int main(){
     node* Head = NULL;
     node* one = new node(1);
@@ -34,7 +62,8 @@ int main(){
     three->next = four;
     four->next = one;
 
+    Insert(Head, 0, 24);
     Display(Head);
-    
+
     return 0;
 }
