290be24f6e4d55e74c9c5cb909da59512ba8d826
Tejas Chaudhari <tmcv0.01@gmail.com>
2023-11-27 09:17:27 +0100

    Add code to sort -ve elements on left

--- Array/array_sort.cpp
+++ Array/array_sort.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 struct Array
@@ -18,21 +18,21 @@ void Display(struct Array arr)
 
 void insertInSortedArray(struct Array *arr, int element)
 {
-    int i = arr->length-1;
+    int i = arr->length - 1;
     arr->length++;
-    while(element < arr->A[i]){
-        arr->A[i+1] = arr->A[i];
+    while (element < arr->A[i])
+    {
+        arr->A[i + 1] = arr->A[i];
         i--;
     }
-    arr->A[i+1] = element;
-    
+    arr->A[i + 1] = element;
 }
 
 bool isSorted(struct Array arr)
 {
-    for(int i=0;i<arr.length-1;i++)
+    for (int i = 0; i < arr.length - 1; i++)
     {
-        if(arr.A[i+1]<arr.A[i])
+        if (arr.A[i + 1] < arr.A[i])
         {
             return false;
         }
@@ -40,13 +40,36 @@ bool isSorted(struct Array arr)
     return true;
 }
 
+void negativeOnLeft(struct Array *arr)
+{
+    int i = 0, j = arr->length-1;
+    while (i < j)
+    {
+
+        while (arr->A[i] < 0)
+        {
+            i++;
+        }
+        while (arr->A[j] >= 0)
+        {
+            j--;
+        }
+        if (i < j)
+        {
+            int temp = arr->A[i];
+            arr->A[i] = arr->A[j];
+            arr->A[j] = temp;
+        }
+    }
+}
+
 int main()
 {
     struct Array arr;
-    arr.length = 4;
-    arr.size = 6;
-    arr.A = new int[arr.size] {1,2,3,5};
-    insertInSortedArray(&arr, 4);
+    arr.length = 10;
+    arr.size = 10;
+    arr.A = new int[arr.size]{-6, 3, -8, 10, 5, -7, -9, 12, -4, 2};
+    negativeOnLeft(&arr);
     Display(arr);
     return 0;
-}
+};
