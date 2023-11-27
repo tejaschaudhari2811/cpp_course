4ac1ca4c3628c7c7cebd84a159d7346d663a300a
Tejas Chaudhari <tmcv0.01@gmail.com>
2023-11-27 09:10:26 +0100

    Add code for inserting in sorted array

--- Array/array_sort.cpp
+++ Array/array_sort.cpp
@@ -8,6 +8,26 @@ struct Array
     int length;
 };
 
+void Display(struct Array arr)
+{
+    for (int i = 0; i < arr.length; i++)
+    {
+        cout << arr.A[i] << endl;
+    }
+}
+
+void insertInSortedArray(struct Array *arr, int element)
+{
+    int i = arr->length-1;
+    arr->length++;
+    while(element < arr->A[i]){
+        arr->A[i+1] = arr->A[i];
+        i--;
+    }
+    arr->A[i+1] = element;
+    
+}
+
 bool isSorted(struct Array arr)
 {
     for(int i=0;i<arr.length-1;i++)
@@ -23,9 +43,10 @@ bool isSorted(struct Array arr)
 int main()
 {
     struct Array arr;
-    arr.length = 5;
-    arr.size = 5;
-    arr.A = new int[arr.size] {1,2,3,4,5};
-    cout<<isSorted(arr)<<endl;
+    arr.length = 4;
+    arr.size = 6;
+    arr.A = new int[arr.size] {1,2,3,5};
+    insertInSortedArray(&arr, 4);
+    Display(arr);
     return 0;
 }
