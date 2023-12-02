180cb9484f40c1c5d4d6470e7fa893b3ac943288
Tejas Chaudhari <tmcv0.01@gmail.com>
2023-12-02 18:02:57 +0100

    Add practice code

--- LinkedList/LList_practice.cpp
+++ LinkedList/LList_practice.cpp
@@ -24,17 +24,65 @@ void Display(Node *head)
     }
 }
 
+void RDisplay(Node *head)
+{
+    // Function to display linkedlist recursively.
+    Node *temp = head;
+    if (temp != nullptr)
+    {
+        cout << temp->data << endl;
+        RDisplay(temp->next);
+    }
+}
 int Count(Node *head)
 {
     // Function to count nodes in a linkedlist.
     Node *temp = head;
     int count = 0;
-    while(temp!=NULL)
+    while (temp != NULL)
     {
         count++;
         temp = temp->next;
     }
     return count;
+}
+
+int RCount(Node *head)
+{
+    Node *temp = head;
+    if (temp == nullptr)
+    {
+        return 0;
+    }
+    else
+    {
+        return RCount(temp->next) + 1;
+    }
+}
+
+int Sum(Node *head)
+{
+    int sum = 0;
+    Node *temp = head;
+    while (temp != nullptr)
+    {
+        sum = sum + temp->data;
+        temp = temp->next;
+    }
+    return sum;
+}
+
+int RSum(Node *head)
+{
+    // Function to calculate sum of nodes recursively.
+    Node *temp = head;
+    if(temp == nullptr)
+    {
+        return 0;
+    }
+    else{
+        return RSum(temp->next) + temp->data;
+    }
 }
 int main()
 {
@@ -48,8 +96,8 @@ int main()
     first->next = second;
     second->next = third;
 
-    Display(head);
-    cout<<"The number of nodes in the linkedlist are "<<Count(head)<<endl;
-
+    RDisplay(head);
+    cout << "The number of nodes in the linkedlist are " << RCount(head) << endl;
+    cout << "The sum of the nodes in linkedlist is " << RSum(head) << endl;
     return 0;
 }
