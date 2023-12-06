fefe0770948ff58e53da22bb1eb3061f616fde9b
Tejas Chaudhari <tmcv0.01@gmail.com>
2023-12-06 10:48:56 +0100

    Add postotder traversal BST

---
+++ BST/postorder_BST.cpp
@@ -0,0 +1,72 @@
+#include<iostream>
+using namespace std;
+
+struct node
+{
+    int key;
+    struct node *left, *right;
+};
+
+struct node* newNode(int item)
+{
+    struct node *temp = (struct node*)malloc(sizeof(struct node));
+    temp->key = item;
+    temp->left = temp->right = nullptr;
+    return temp;
+}
+
+struct node* insert(struct node* node, int key)
+{
+    if(node==nullptr)
+    {
+        return newNode(key);
+    }
+    if(key<node->key)
+    {
+        node->left = insert(node->left,key);
+    }
+    else if (key>node->key)
+    {
+        node->right = insert(node->right, key);
+    }
+    return node;
+}
+
+void postorder(struct node* node)
+{
+    if(node != nullptr)
+    {
+        postorder(node->left);
+        postorder(node->right);
+        cout<<node->key<<endl;
+    }
+}
+
+// Driver Code
+int main()
+{
+     
+    /* Let us create following BST 
+              50 
+           /     \ 
+          30      70 
+         /  \    /  \ 
+       20   40  60   80 
+   */
+    struct node* root = NULL;
+ 
+    // Creating the BST
+    root = insert(root, 50);
+    insert(root, 30);
+    insert(root, 20);
+    insert(root, 40);
+    insert(root, 70);
+    insert(root, 60);
+    insert(root, 80);
+ 
+    // Function Call
+    postorder(root);
+ 
+    return 0;
+}
+ 
