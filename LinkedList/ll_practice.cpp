30907ec30667bac70920a044ed61428a396c23b1
Tejas Chaudhari <tmcv0.01@gmail.com>
2023-12-09 19:25:28 +0100

    Add insert at head

--- LinkedList/ll_practice.cpp
+++ LinkedList/ll_practice.cpp
@@ -38,6 +38,23 @@ void InsertAtTail(node* head, int element)
     temp->next = new_node;
 }
 
+void InsertAtHead(node* &head, int element)
+{
+    node *new_node = new node(element);
+    new_node->next = head;
+    head = new_node;
+}
+
+void Delete(node *head)
+{
+    node *temp = head;
+    while(temp->next->next !=nullptr)
+    {
+        temp = temp->next;
+    }
+    temp->next = nullptr;
+}
+
 int main()
 {
     node *head = nullptr;
@@ -50,7 +67,8 @@ int main()
     two->next = three;    
 
     head = one;
-    InsertAtTail(head, 20);
+    InsertAtHead(head, 20);
+    Delete(head);
     Display(head);
     return 0;
 };
