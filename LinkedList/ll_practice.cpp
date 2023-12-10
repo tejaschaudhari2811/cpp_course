149f6f212e024ecaacff5a30a00d056c92f3cb3b
Tejas Chaudhari <tmcv0.01@gmail.com>
2023-12-10 11:04:33 +0100

    Add insert in sorted LList

--- LinkedList/ll_practice.cpp
+++ LinkedList/ll_practice.cpp
@@ -18,37 +18,50 @@ public:
 
 void Display(node *head)
 {
-    node* temp = head;
-    while(temp !=NULL)
+    node *temp = head;
+    while (temp != NULL)
     {
-        cout<<temp->data<<endl;
+        cout << temp->data << endl;
         temp = temp->next;
     }
 }
 
-void InsertAtTail(node* head, int element)
+void InsertAtTail(node *head, int element)
 {
     // Here assume that node is inserted at end
-    node * new_node = new node(element);
+    node *new_node = new node(element);
     node *temp = head;
-    while(temp->next!=NULL)
+    while (temp->next != NULL)
     {
         temp = temp->next;
     }
     temp->next = new_node;
 }
 
-void InsertAtHead(node* &head, int element)
+void InsertAtHead(node *&head, int element)
 {
     node *new_node = new node(element);
     new_node->next = head;
     head = new_node;
 }
 
+void InsertSorted(node* &head, int element)
+{
+    node* new_node = new node(element);
+    node* temp = head;
+    while(temp->next->data < new_node->data)
+    {
+        temp = temp->next;
+    }
+    new_node->next = temp->next;
+    temp->next = new_node;
+    return;
+}
+
 void Delete(node *head)
 {
     node *temp = head;
-    while(temp->next->next !=nullptr)
+    while (temp->next->next != nullptr)
     {
         temp = temp->next;
     }
@@ -58,17 +71,16 @@ void Delete(node *head)
 int main()
 {
     node *head = nullptr;
-    node *one = new node(1);
-    node *two = new node(2);
-    node *three = new node(3);
+    node *one = new node(2);
+    node *two = new node(4);
+    node *three = new node(6);
 
     // Connect the nodes
     one->next = two;
-    two->next = three;    
+    two->next = three;
 
     head = one;
-    InsertAtHead(head, 20);
-    Delete(head);
+    InsertSorted(head, 3);
     Display(head);
     return 0;
 };
