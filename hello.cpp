5368af2e993393f6a0c2edb071b5b57b3d03e84e
Tejas Chaudhari <tmcv0.01@gmail.com>
2023-12-12 13:09:20 +0100

    Compiler working again

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,7 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-
 int main(){
-    cout<<"Hello world"<<endl;
+    cout<<"Hello Tejas"<<endl;
     return 0;
 }
---
+++ helloworld.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+
+int main()
+{
+    std::cout<<"Program to check if the code works."<<std::endl;
+    return 0;
+}
