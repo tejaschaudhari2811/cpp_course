3ff75bdf163797d540f3ad38ea5e83fc808417ad
Tejas Chaudhari <tmcv0.01@gmail.com>
2024-01-06 09:03:08 +0100

    Add code for selecetion sort

---
+++ Sorting/selectionSort2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+void swap(int &a, int &b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+void selectionSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        int k = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[k])
+            {
+                k = j;
+            }
+        }
+        swap(arr[i], arr[k]);
+    }
+}
+int main()
+{
+    int arr[] = {10, 2, 3, 502, 5, 2};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    selectionSort(arr, n);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << endl;
+    }
+}
