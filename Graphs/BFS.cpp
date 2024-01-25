53ce6637868bc13b50104b831485cb3d6ef262ad
Tejas Chaudhari <tmcv0.01@gmail.com>
2024-01-25 08:22:44 +0100

    Add BFS for graph

---
+++ Graphs/BFS.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+class Graph
+{
+    int V; // Number of vertices
+
+public:
+    vector<list<int>> adj;
+
+    Graph(int V);
+
+    void addEdge(int v, int w);
+
+    void BFS(int s);
+};
+
+Graph::Graph(int v)
+{
+    this->V = v;
+    adj.resize(V);
+}
+
+void Graph::addEdge(int v, int w)
+{
+    adj[v].push_back(w);
+}
+
+void Graph::BFS(int s)
+{
+    vector<bool> visited;
+    visited.resize(V, false);
+
+    list<int> queue;
+
+    // Mark the current node as visited.
+    visited[s] = true;
+    queue.push_back(s);
+
+    while (!queue.empty())
+    {
+        s = queue.front();
+        cout << s << " ";
+        queue.pop_front();
+
+        for (auto adjacent : adj[s])
+        {
+            if (!visited[adjacent])
+            {
+                visited[adjacent] = true;
+                queue.push_back(adjacent);
+            }
+        }
+    }
+}
+int main()
+{
+    // Create a graph given in the above diagram
+    Graph g(4);
+    g.addEdge(0, 1);
+    g.addEdge(0, 2);
+    g.addEdge(1, 2);
+    g.addEdge(2, 0);
+    g.addEdge(2, 3);
+    g.addEdge(3, 3);
+
+    cout << "Following is Breadth First Traversal "
+         << "(starting from vertex 2) \n";
+    g.BFS(2);
+
+    return 0;
+}
